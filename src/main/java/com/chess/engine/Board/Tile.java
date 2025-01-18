a994739ded01ba544650336e09a2646bc5c51690
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2025-01-18 14:27:23 +0100

    Add factory methode to generate Tile
    Add an immutable map of empty Tiles to utilize
    during Tile creation
    Made sure I implemented immutability in my code
    for thread safety
    Applied factory design pattern to create Tiles
    using createTile() factory method

--- src/main/java/com/chess/engine/Board/Tile.java
+++ src/main/java/com/chess/engine/Board/Tile.java
@@ -1,16 +1,41 @@
 package com.chess.engine.Board;
 import com.chess.engine.Pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Tile {
 
-    int tileCoordinate;
-    Tile(int tileCoordinate) {
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile > EMPTY_TILES = createAllPossibleEmptyTiles();
+
+
+    // create all possible Empty Tiles to utilise Immutability
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        Map<Integer, EmptyTile> emptyTiles = new HashMap<Integer, EmptyTile>();
+        for ( int i = 0 ; i < 64 ; ++i )  {
+            emptyTiles.put(i, new EmptyTile(i));
+        }
+        return ImmutableMap.copyOf(emptyTiles);
+
+    }
+
+    // factory methode to generate Tiles
+    public static Tile CreateTile(int tileCoordinate , Piece piece) {
+        return piece == null ? EMPTY_TILES.get(tileCoordinate) : new OccupiedTile(tileCoordinate, piece);
+    }
+
+    private Tile(final int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
     public abstract boolean isTileOccupied();
     public abstract Piece getPiece();
 
+
+    // Empty tile class - static keyword is used to prevent this class from inheriting from the enclosing class
+    // it's treated as independent, logical components of the Tile class. we can instantiate EmptyTile and OccupiedTile without needing an instance of the enclosing Tile class.
     public static final class EmptyTile extends Tile {
         EmptyTile(int coordinate) {
             super(coordinate);
