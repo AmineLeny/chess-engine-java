2b558e95b7f3bbab67c28ee459eb0457b70a4da6
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2025-01-26 06:01:05 +0100

    Add Table class in gui

--- src/main/java/com/chess/gui/Table.java
+++ src/main/java/com/chess/gui/Table.java
@@ -2,6 +2,8 @@ package com.chess.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Table {
     private static final Dimension OUTER_FRAM_DIMENSION = new Dimension(600,600) ;
@@ -9,8 +11,30 @@ public class Table {
 
     public Table() {
         this.gameFrame = new JFrame("Chess");
+        final JMenuBar tableMenuBar = new JMenuBar();
+        populateMenuBar(tableMenuBar);
+        this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAM_DIMENSION);
         this.gameFrame.setVisible(true);
         this.gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void populateMenuBar(final JMenuBar tableMenuBar) {
+        tableMenuBar.add(createFileMenu());
+    }
+
+    private JMenu createFileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+        final JMenuItem openPGN = new JMenuItem("Load PGN File");
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                System.out.println("Opening PGN File...");
+            }
+        });
+
+        fileMenu.add(openPGN);
+        return fileMenu;
+
     }
 }
