e32bc860c60c156ba34a58e3a47c2eaeea8a6c76
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2025-01-26 15:45:13 +0100

    Assign Pieces in board Tile UI using assignTilePieceIcon()
    Add art folder containing our images
    Implement the logic for assignTilePieceIcon()

--- src/main/java/com/chess/gui/Table.java
+++ src/main/java/com/chess/gui/Table.java
@@ -1,11 +1,18 @@
 package com.chess.gui;
 
+import com.chess.engine.Alliance;
 import com.chess.engine.Board.Board;
 import com.chess.engine.Board.BoardPosition;
 import com.chess.engine.Board.Tile;
+import com.chess.engine.Pieces.Piece;
+import com.chess.engine.Pieces.Piece.PieceType;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -67,14 +74,17 @@ public class Table {
     private class BoardPanel extends JPanel {
 
         final List<TilePanel> boardTiles ;
-
+        final Board board;
         BoardPanel(){
+
             super(new GridLayout(8,8));
+             board = Board.createStandardBoard();
             this.boardTiles = new ArrayList<>();
 
                 for (int i = 7; i >=0 ; --i) {
                     for (int j = 0 ; j < 8 ; ++j) {
-                        final TilePanel tilePanel = new TilePanel(this , new BoardPosition(i,j));
+                        final TilePanel tilePanel = new TilePanel(this , new BoardPosition(j,i),board);
+
                         this.boardTiles.add(tilePanel);
                         this.add(tilePanel);
                     }
@@ -91,15 +101,62 @@ public class Table {
         private final BoardPosition tileId;
 
         TilePanel(final BoardPanel boardPanel,
-                  final BoardPosition tileId
+                  final BoardPosition tileId,
+                  final Board board
                   ) {
             super(new GridBagLayout());
             this.tileId = tileId;
             setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor(tileId );
+            assignTilePieceIcon(board);
             validate();
 
         }
+
+        private void assignTilePieceIcon(final Board board) {
+            this.removeAll();
+            if (board.getTile(this.tileId).isTileOccupied()) {
+                Piece piece = board.getTile(this.tileId).getPiece();
+                PieceType pieceType = piece.getPieceType();
+                Alliance pieceAlliance = piece.getPieceAlliance();
+                String piecePath ;
+                if(pieceAlliance.isWhite()) {
+                   piecePath = switch (pieceType) {
+                        case PAWN ->  "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\BP.gif";
+                        case KING ->   "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\BK.gif" ;
+                        case ROOK ->   "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\BR.gif";
+                        case QUEEN ->  "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\BQ.gif";
+                        case BISHOP -> "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\BB.gif";
+                        case KNIGHT -> "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\BN.gif" ;
+
+                    };
+                }
+                else {
+                    piecePath = switch (pieceType) {
+                        case PAWN ->  "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\WP.gif";
+                        case KING ->  "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\WK.gif" ;
+                        case ROOK ->  "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\WR.gif";
+                        case QUEEN -> "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\WQ.gif";
+                        case BISHOP ->"C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\WB.gif";
+                        case KNIGHT -> "C:\\Users\\amine\\Documents\\Chess Engine\\Chess Engine\\art\\fancy\\WN.gif" ;
+
+                    };
+                }
+
+
+                try{
+                    final BufferedImage image = ImageIO.read(new File(
+                                    piecePath
+                            )
+                    );
+                    this.add(new JLabel(new ImageIcon(image)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
 
         private void assignTileColor(BoardPosition tileId) {
             boolean isDarkTile = (tileId.x()+ tileId.y()) % 2 == 0;
